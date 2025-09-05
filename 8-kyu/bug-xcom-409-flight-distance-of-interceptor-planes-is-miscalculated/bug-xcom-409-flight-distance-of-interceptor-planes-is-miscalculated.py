a887c98d5ea370ed797e7e6d1e64fcfe465dd5c5
Alesson Oliveira <alessonmachado14@gmail.com>
2025-09-05 12:45:58 -0300

    Add solution - CodeHub

---
+++ 8-kyu/bug-xcom-409-flight-distance-of-interceptor-planes-is-miscalculated/bug-xcom-409-flight-distance-of-interceptor-planes-is-miscalculated.py
@@ -0,0 +1,6 @@
+# speed of aircrafts is given in *knots* 1852kmph
+# travelTime is in *minutes*
+# travel distance should be returned in *kilometers*
+​
+def travel_distance(avg_speed, travel_time):
+        return (travel_time / 60) * avg_speed * 1.852
