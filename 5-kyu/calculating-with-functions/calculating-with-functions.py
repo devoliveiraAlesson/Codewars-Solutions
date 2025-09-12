b79a0eb15e7220d3db5d0f868132b6cf5ff05790
Alesson Oliveira <alessonmachado14@gmail.com>
2025-09-12 20:42:37 -0300

    Add solution - CodeHub

---
+++ 5-kyu/calculating-with-functions/calculating-with-functions.py
@@ -0,0 +1,19 @@
+def zero(x=""): return eval(f"0{x}")//1
+def one(x=""): return eval(f"1{x}")//1
+def two(x=""): return eval(f"2{x}")//1
+def three(x=""): return eval(f"3{x}")//1
+def four(x=""): return eval(f"4{x}")//1
+def five(x=""): return eval(f"5{x}")//1
+def six(x=""): return eval(f"6{x}")//1
+def seven(x=""): return eval(f"7{x}")//1
+def eight(x=""): return eval(f"8{x}")//1
+def nine(x=""): return eval(f"9{x}") //1
+​
+def plus(x): return f"+{x}"
+def minus(x): return f"-{x}"
+def times(x): return f"*{x}"
+def divided_by(x): return f"/{x}"
+​
+​
+​
+​
