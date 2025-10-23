c136fda32f604e366242f2b1c7732226227813ce
Alesson Oliveira <alessonmachado14@gmail.com>
2025-10-23 10:31:23 -0300

    Add solution - CodeHub

---
+++ 7-kyu/filtering-films-by-special-features-in-postgresql-part-3/filtering-films-by-special-features-in-postgresql-part-3.sql
@@ -0,0 +1,18 @@
+ 
+with filmes as (
+select
+  film_id,
+  title,
+  
+  case
+    when 'Commentaries' = any(special_features) then NUll
+    when 'Deleted Scenes' = any(special_features) and 'Behind the Scenes'= any(special_features) then NUll
+    when 'Deleted Scenes' = any(special_features) or 'Behind the Scenes'= any(special_features) then special_features
+    else NUll end
+    as special_features
+​
+from film
+)
+​
+select * from filmes
+where special_features not Null
